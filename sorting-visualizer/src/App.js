9b610be6eaa67df37e00f888986514afcb3e1a39
kevinsims1 <kevinsims1@outlook.com>
2019-11-25 09:17:10 -0600

    array of bars rendering

---
+++ sorting-visualizer/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import SortingVisualizer from './SortingVisualizer.js'
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer />
+    </div>
+  );
+}
+
+export default App;
